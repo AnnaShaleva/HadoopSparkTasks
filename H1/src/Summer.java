5b98a683a51cd72387a798715794339343a70f27
Anna Shaleva <shaleva.ann@gmail.com>
2021-04-10 16:57:40 +0300

    H1: add task1

---
+++ H1/src/Summer.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.*;
+
+public class Summer extends Reducer<Text, IntWritable, Text, IntWritable> {
+  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+       
+  }
+}
